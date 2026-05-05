c38b733f6c340114596a5cf7d9b541e8c409458b
DIOOWS <dioowdioowtrd@gmail.com>
2026-05-05 10:45:22 -0300

    fix: rota editar manutencoes

--- routes/manutencoes.py
+++ routes/manutencoes.py
@@ -73,3 +73,46 @@ def excluir(id):
     flash("Manutenção excluída com sucesso!", "success")
 
     return redirect("/manutencoes")
+
+
+@manutencao_bp.route("/editar/<int:id>", methods=["GET", "POST"])
+def editar(id):
+
+    if not session.get("user_id"):
+        return redirect("/login")
+
+    m = Manutencao.query.get_or_404(id)
+
+    if request.method == "POST":
+
+        data = request.form.get("data")
+
+        try:
+            m.data = datetime.strptime(data, "%Y-%m-%d") if data else None
+        except:
+            m.data = None
+
+        m.numero_frota = request.form.get("numero_frota")
+        m.bau = request.form.get("bau")
+        m.tipo_veiculo = request.form.get("tipo_veiculo")
+        m.tipo_servico = request.form.get("tipo_servico")
+        m.tipo_atendimento = request.form.get("tipo_atendimento")
+        m.tipo_manutencao = request.form.get("tipo_manutencao")
+        m.status = request.form.get("status")
+        m.observacao = request.form.get("observacao")
+        m.cliente = request.form.get("cliente")
+        m.os = request.form.get("os")
+
+        db.session.commit()
+
+        flash("Manutenção atualizada com sucesso!", "success")
+
+        return redirect("/frotas/" + str(m.numero_frota))
+
+    clientes = Cliente.query.order_by(Cliente.nome).all()
+
+    return render_template(
+        "manutencoes/form.html",
+        m=m,
+        clientes=clientes
+    )
